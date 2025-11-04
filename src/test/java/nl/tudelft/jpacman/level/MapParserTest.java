ac3df629617758c939feac770442801844e9be48
aueynattapat <nattapat.tho@student.mahidol.ac.th>
2025-11-04 21:36:14 +0700

    update build.gradle and Add MapParsertest

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -2,7 +2,6 @@ package nl.tudelft.jpacman.level;
 
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -36,7 +35,9 @@ public class MapParserTest {
         assertNotNull(boardFactory);
         assertNotNull(levelFactory);
         Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
+
         MapParser mapParser = new MapParser(levelFactory, boardFactory);
+
         ArrayList<String> map = new ArrayList<>();
         map.add("############");
         map.add("#P        G#");
