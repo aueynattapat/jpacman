0012e5008213ca08b3342b0023623a0ea5dbb21f
NattapatThongrattana <109589098+aueynattapat@users.noreply.github.com>
2025-11-04 15:21:17 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -9,7 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * Tests that a Board with a valid grid of Squares (created successfully).
  */
 class BoardTest {
-
+    /**
+    *
+    */
     @Test
     void testValidBoard() {
         // 1x1 grid with one valid BasicSquare
@@ -25,6 +27,9 @@ class BoardTest {
         assertThat(board.squareAt(0, 0)).isEqualTo(grid[0][0]);
         assertThat(board.withinBorders(0, 0)).isTrue();
     }
+    /**
+    *
+    */
     @Test
     void testInvalidBoardNullSquare() {
         Square[][] grid = new Square[1][1];
